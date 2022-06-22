02985f667989130cbd3049f7b2432e5b124e92f1
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-06-22 22:31:35 +0300

    Update ContactList.jsx

--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,36 +1,38 @@
-import s from './ContactList.module.css';
-       
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteContact, getFilter, getItems } from 'redux/contactsSlice';
+import s from './ContactList.module.css';      
+import { useSelector } from 'react-redux';
+import { getFilter } from 'redux/contactsSlice';
+import { useGetContactsQuery, useDeleteContactMutation } from 'redux/contactsApi';
 
   const ContactList = () => {
-  const contacts = useSelector(getItems);
+  const { data} = useGetContactsQuery();
+  const [deleteContact] = useDeleteContactMutation();
   const filter = useSelector(getFilter);
-  const dispatch = useDispatch();
+ 
 
   const filteredContacts = () => {
-    return contacts.filter(contact =>
+    return data.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
-  let rendered = filter === '' ? contacts : filteredContacts();
+  let rendered = filter === '' ? data : filteredContacts();
   return (
     <>
       <ul className={s.list}>
-        {rendered.map(({ name, id, number }) => (
+        {data &&
+          rendered.map(({ name, id, number }) => (
           <li className={s.item} key={id} id={id}>
             {name}: {number}
 
             <button
               type="button"
               className={s.button}
-              onClick={e => dispatch(deleteContact(e.currentTarget.parentNode.id))}>
+              onClick={() => deleteContact(id)}>
             Delete
             </button>
           </li>
         ))}
       </ul>
-      {contacts.length === 0 && (
+      {data && data.length === 0 && (
         <p>no contacts available</p>
       )}
     </>
