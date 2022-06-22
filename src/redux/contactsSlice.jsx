441c8db251edfa69336e2c9f6e1f62c755dd2c46
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-06-22 22:20:32 +0300

    Update contactsSlice.jsx

--- src/redux/contactsSlice.jsx
+++ src/redux/contactsSlice.jsx
@@ -1,8 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
-const contactsSlice = createSlice({
+export const contactsSlice = createSlice({
   name: 'contacts',
   initialState: {
     items: [],
@@ -26,16 +24,6 @@ const contactsSlice = createSlice({
 });
 
 export const { addContacts, deleteContact, setFilter } = contactsSlice.actions;
-
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-export const persistedContactsReducer = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
 
 export const getItems = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
